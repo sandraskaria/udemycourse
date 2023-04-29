8de55a1a8c3082d4e04d3a2d51d79455a664bc8a
sandra.skaria <sandra.skaria@quest-global.com>
2023-04-29 15:28:18 +0530

    ViewModel Factory sample

--- app/src/main/java/com/example/viewmodel/ModelActivityViewModel.kt
+++ app/src/main/java/com/example/viewmodel/ModelActivityViewModel.kt
@@ -3,7 +3,11 @@ package com.example.viewmodel
 import androidx.lifecycle.ViewModel
 
 class ModelActivityViewModel(val startValue: Int): ViewModel() {
-    private var count = startValue
+    private var count = 0
+
+    init {
+        count = startValue
+    }
 
     fun doAddition(number: Int) {
        // count = number?.plus(count) ?: 0
